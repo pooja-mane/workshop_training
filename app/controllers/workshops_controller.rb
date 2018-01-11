2b956fc56a1669075ddf190a2f7a0dfc18d22389
pooja-mane <poojamane1@gmail.com>
2018-01-11 17:25:12 +0530

    - Added validation for workshop - Added workshop controller and views for CRUD.

---
+++ app/controllers/workshops_controller.rb
@@ -0,0 +1,57 @@
+class WorkshopsController < ApplicationController
+  before_action :get_workshop, only: [:show, :edit, :update, :destroy]
+
+  # GET /workshops
+  def index
+    @workshops = Workshop.all
+  end
+
+  # GET /workshops/1
+  def show
+  end
+
+  # GET /workshops/new
+  def new
+    @workshop = Workshop.new
+  end
+
+  # GET /workshops/1/edit
+  def edit
+  end
+
+  # Workshops /workshops
+  def create
+    @workshop = Workshop.new(workshop_params)
+    if @workshop.save
+      redirect_to root_path, notice: 'Workshop was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /workshops/1
+  def update
+    if @workshop.update(workshop_params)
+      redirect_to @workshop, notice: 'Workshop was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /workshops/1
+  def destroy
+    @workshop.destroy
+    redirect_to workshops_url, notice: 'Workshop was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def get_workshop
+      @workshop = Workshop.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def workshop_params
+      params.require(:workshop).permit(:title, :description, :start_date, :end_date, :place)
+    end
+end
--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -2,4 +2,6 @@ class Workshop < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :topics
   has_and_belongs_to_many :coaches
+
+  validates :title, :description, :place, presence: true
 end
