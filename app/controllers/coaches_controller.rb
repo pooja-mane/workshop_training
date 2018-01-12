acf601c1ada2c896db40bb7e1d8c7f48659f712c
pooja-mane <poojamane1@gmail.com>
2018-01-12 11:44:30 +0530

    Added CRUD for coach

---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,51 @@
+class CoachesController < ApplicationController
+
+  before_action :load_coach, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @coaches = Coach.all
+  end
+
+  def new
+    @coach = Coach.new    
+  end
+
+  def create
+    p params[:coach], "================"
+    @coach = Coach.new(coach_params)
+    if @coach.save
+      redirect_to coaches_path, notice: 'Coach was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @coach.update(coach_params)
+      redirect_to coach_path(@coach), notice: 'Coach was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @coach.destroy
+    redirect_to coaches_path, notice: 'Coach was successfully destroyed.'          
+  end
+
+  private
+
+  def coach_params
+    params.require(:coach).permit(:name, :email)
+  end
+
+  def load_coach
+    @coach = Coach.find(params[:id])    
+  end
+end
