0808897b837c4465dc06185ee3f0444f65bdd103
pooja-mane <poojamane1@gmail.com>
2018-01-12 15:37:32 +0530

    - Added input field for start_date and end_date. - Added before_save callback for workshop.

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -3,5 +3,19 @@ class Workshop < ApplicationRecord
   has_many :topics
   has_and_belongs_to_many :coaches
 
-  validates :title, :description, :place, presence: true
+  validates :title, :description, :place, :start_date, :end_date, presence: true
+
+  before_save :calculate_duration, if: -> { start_date_changed? || end_date_changed? }
+
+  private
+
+  def calculate_duration
+    days ||= if self.end_date > self.start_date
+      (self.end_date.to_date - self.start_date.to_date).to_i
+    else
+      0
+    end rescue 0
+    self.duration = days
+    # self.duration = (self.end_date.to_date - self.start_date.to_date).to_i
+  end
 end
